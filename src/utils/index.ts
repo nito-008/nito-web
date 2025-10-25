7369167653407f33ed5dca9f765225272936e12c
Nito <100199931+Nitoryu008@users.noreply.github.com>
2025-10-25 14:49:23 +0900

    refactor: astro v5に対応

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -21,26 +21,3 @@ export function splitText(target: string | Element) {
 
   return target.querySelectorAll("span");
 }
-
-import type { CollectionEntry } from "astro:content";
-import { getCollection } from "astro:content";
-
-export async function getPosts(): Promise<CollectionEntry<"blog">[]> {
-  const posts = await getCollection("blog");
-
-  posts.sort((a, b) => {
-    return b.data.pubDate.getTime() - a.data.pubDate.getTime();
-  });
-
-  return posts;
-}
-
-export async function getWorks(): Promise<CollectionEntry<"work">[]> {
-  const posts = await getCollection("work");
-
-  posts.sort((a, b) => {
-    return b.data.pubDate.getTime() - a.data.pubDate.getTime();
-  });
-
-  return posts;
-}
